9cc3afbc40eac6d8ad9b9d559ee71187e26b48d8
Sasindu Nadeeshan <Sasindun2000@gmail.com>
2024-02-03 11:59:59 +0530

    make it better

--- components/sub/HeroContent.tsx
+++ components/sub/HeroContent.tsx
@@ -1,62 +1,66 @@
-"use client"
-import React from 'react'
-import{motion} from 'framer-motion'
-import { slideInFromLeft, slideInFromTop } from '@/utils/motion'
-import { SparklesIcon } from '@heroicons/react/16/solid'
+"use client";
+import React from 'react';
+import { motion } from 'framer-motion';
+import { slideInFromLeft, slideInFromTop } from '@/utils/motion';
+import { SparklesIcon } from '@heroicons/react/16/solid';
 
 const HeroContent = () => {
-    return (
-      <motion.div
-        initial="hidden"
-        animate="visible"
-        className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
-      >
-        <div className="h-full w-full flex flex-col gap-5 justify-center mx-auto text-start">
-          <motion.div
-            variants={slideInFromTop}
-            className="Welcome-box py-[8px] px-[7px] border border-[#7042f88b] opacity-[0.9]"
-          >
-            <SparklesIcon className="text-[#b49bff] mr-[10px] h-5 w-5" />
-            <h1 className="Welcome-text text-[13px]">
-              Welcome to my Portfolio
-            </h1>
-          </motion.div>
-
-          <motion.div
-          variants ={slideInFromLeft(0.5)}
-          className='flex flex-col gap-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto'
-          >
-            <span>
-            Hi,
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500'> I'm  </span>
-            Sasindu Nadeeshan
-            </span>
-          </motion.div>
-
-          <motion.p
-          variants={slideInFromLeft(0.8)}
-          className='text-lg text-gray-400 my-5 max-w-[600px]'>
-            I'm, a full-stack developer passionate about creating dynamic and user-friendly web applications. With expertise in both frontend and backend technologies, I bring ideas to life by combining creativity with solid technical skills. Let's build something incredible together!
-          </motion.p>
-          <motion.a
-          variants={slideInFromLeft(1)}
-          className='py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]'
-          >
-            Learn More!
-          </motion.a>
-        </div>
+  return (
+    <motion.div
+      initial="hidden"
+      animate="visible"
+      className="flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]"
+    >
+      <div className="h-full w-full flex flex-col gap-5 justify-center mx-auto text-start">
         <motion.div
-        variants={slideInFromLeft(0.8)}
-        className='w-full h-full flex justify-center item-center'
+          variants={slideInFromTop}
+          className="Welcome-box py-8 px-7 border border-[#7042f88b] opacity-0.9"
         >
-            <img
-            src="/mainIconsdark.svg"
-            alt="work icons"
-            height={650}
-            width={650}/>
+          <SparklesIcon className="text-[#b49bff] mr-10 h-5 w-5" />
+          <h1 className="Welcome-text text-13">
+            Welcome to my Portfolio
+          </h1>
         </motion.div>
+
+        <motion.div
+          variants={slideInFromLeft(0.5)}
+          className="flex flex-col gap-6 text-6xl font-bold text-white max-w-[600px] w-auto h-auto"
+        >
+          <span>
+            Hi,
+            <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500"> I'm </span>
+            Sasindu Nadeeshan
+          </span>
+        </motion.div>
+
+        <motion.p
+          variants={slideInFromLeft(0.8)}
+          className="text-lg text-gray-400 my-5 max-w-[600px]"
+        >
+          I'm, a full-stack developer passionate about creating dynamic and user-friendly web applications. With expertise in both frontend and backend technologies, I bring ideas to life by combining creativity with solid technical skills. Let's build something incredible together!
+        </motion.p>
+
+        <motion.a
+          variants={slideInFromLeft(1)}
+          className="py-2 button-primary text-center text-white cursor-pointer rounded-lg max-w-[200px]"
+        >
+          Learn More!
+        </motion.a>
+      </div>
+
+      <motion.div
+        variants={slideInFromLeft(0.8)}
+        className="w-full h-full flex justify-center items-center"
+      >
+        <img
+          src="/mainIconsdark.svg"
+          alt="work icons"
+          height={650}
+          width={650}
+        />
       </motion.div>
-    );
-  };
+    </motion.div>
+  );
+};
 
 export default HeroContent;
